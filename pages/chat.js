093e707de655dad4dd0fc64e2a81150a362aa339
Dudaqfigueiredo <dudaqfigueiredo@gmail.com>
2022-02-03 12:20:25 -0300

    LinkNomeUsuário2

--- pages/chat.js
+++ pages/chat.js
@@ -297,7 +297,7 @@ function MessageList(props) {
                   flexDirection: 'row',
                 }}
               >
-                <a href={`https://github.com/${mensagem.de}.png`} target="_blank">
+                <a href={`https://github.com/${mensagem.de}`} target="_blank">
                   <Image
                     styleSheet={{
                       width: '30px',
@@ -313,7 +313,7 @@ function MessageList(props) {
                     src={`https://github.com/${mensagem.de}.png`}
                   />
                 </a>
-                <a href={`https://github.com/${mensagem.de}.png`} target="_blank">
+                <a href={`https://github.com/${mensagem.de}`} target="_blank">
                   <Text tag="strong">
                     <Text
                       tag="a"
