be079f8d9ad0b11ea18a4cc8e6aec872eeda9754
Dudaqfigueiredo <dudaqfigueiredo@gmail.com>
2022-02-09 09:45:18 -0300

    Music

--- pages/chat.js
+++ pages/chat.js
@@ -375,7 +375,7 @@ function Musica() {
         {`
           audio {
             margin-top: 1%;
-            margin-left: 50%;
+            margin-left: 2%;
           }
           audio::-webkit-media-controls-enclosure {
             background-color: #bd4cff63;
